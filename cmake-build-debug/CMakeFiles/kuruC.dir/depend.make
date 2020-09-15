a1ab12d5acc27296db37870de8e278fa79287e99
Ryuji Oda <ryuji.oda@gmail.com>
2020-09-15 23:26:09 +0900

    initial commit

---
+++ cmake-build-debug/CMakeFiles/kuruC.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for kuruC.
+# This may be replaced when dependencies are built.
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello\tWorld!\n");
+    printf("kuruC\ttanoC\n");
+    printf("%d+%d=%d円\n",200,100,200+100);
+    printf("%f\n", 10.0/3.0);
+    return 0;
+}
